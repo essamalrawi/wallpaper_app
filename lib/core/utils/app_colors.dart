02118c467999609cb11ff2a0c904fa7e8f1972b6
essam <essamhamdy198@gmail.com>
2025-03-19 20:13:35 +0200

    add app icon, colors, app name

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xFF29323B);
+  static const Color lightPrimaryColor = Color(0xF9F9F9F9);
+  static const Color secondPrimaryColor = Color(0xF9999999);
+}
