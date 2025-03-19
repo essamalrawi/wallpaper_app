24e7dd87cc5da86ceee80194bfd4fabd4b849cf4
essam <essamhamdy198@gmail.com>
2025-03-19 23:14:56 +0200

    test

---
+++ lib/core/utils/app_text_styles.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+abstract class TextStyles {
+  static const TextStyle bold50 = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 50,
+  );
+  static const TextStyle semibold46 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 46,
+  );
+  static const TextStyle semibold34 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 34,
+  );
+  static const TextStyle light32 = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 32,
+  );
+  static const TextStyle regular35 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 35,
+  );
+  static const TextStyle regular22 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 23,
+  );
+  static const TextStyle regular23 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 23,
+  );
+  static const TextStyle medium30 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 30,
+  );
+  static const TextStyle medium26 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 27,
+  );
+}
